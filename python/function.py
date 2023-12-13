144782fd3afb21e41bc5f572a363efe2ba1f10ac
Michaelunkai <michaelovsky5@gmail.com>
2023-12-13 21:24:51 +0200

    try

---
+++ python/function.py
@@ -0,0 +1,46 @@
+# # function = a block of code which is executed only when it is called.
+
+# def hello(first_name,last_name,age):
+#     print('hello '+first_name+' '+last_name)
+#     print('you are '+str(age)+' years old')
+#     print('have a nice day!!')
+
+# hello('misha','fedro',21)
+# # output:
+# # hello misha fedro
+# # you are 21 years old
+# # have a nice day!!
+
+# def hello():
+#     print('hello!')
+
+# hello()
+# hello()
+# hello()
+
+# output:
+# hello!
+# hello!
+# hello!
+
+# def hello():
+#     print('hello!')
+#     print('have a nice day!')
+# hello()
+# hello()
+# hello()
+
+# output:
+# hello!
+# have a nice day!
+# hello!
+# have a nice day!
+# hello!
+# have a nice day!
+
+
+def hello():
+    print('hello! '+name)
+    print('have a nice day!')
+
+hello()
