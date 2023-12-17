fb6404a4fde72de75da695e5001136c65b9ee62f
Michaelunkai <michaelovsky5@gmail.com>
2023-12-17 10:31:59 +0200

    1

---
+++ python/todo.py
@@ -0,0 +1,11 @@
+# Step 1: Setting up the Project
+
+# Import necessary libraries
+import tkinter as tk
+
+# Create the main application window
+app = tk.Tk()
+app.title
+
+# Run the main event loop
+app.mainloop()
