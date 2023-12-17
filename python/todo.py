f30d8f7f1d668585676467e4318aefbd0c7f0f88
Michaelunkai <michaelovsky5@gmail.com>
2023-12-17 12:40:30 +0200

    2

--- python/todo.py
+++ python/todo.py
@@ -5,7 +5,13 @@ import tkinter as tk
 
 # Create the main application window
 app = tk.Tk()
-app.title
+app.title("Task Manager")
 
 # Run the main event loop
 app.mainloop()
+
+# Create a title label
+title_label = tk.Label(app, text="To-Do List", font=("Helvetica", 16, "bold"))
+
+# Pack the title label into the window
+title_label.pack(pady=10)
