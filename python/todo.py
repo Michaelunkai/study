faa5ec39e6a23ede647fbce63ecee7dbee9f6e23
Michaelunkai <michaelovsky5@gmail.com>
2023-12-19 00:18:14 +0200

    1

--- python/todo.py
+++ python/todo.py
@@ -1,24 +1,6 @@
-# Step 1: Setting up the Project
+import flet as ft
 
-# Import necessary libraries
-import tkinter as tk
+def main(page: ft.Page):
+    page.add(ft.Text(value="Hello, world!"))
 
-# Create the main application window
-app = tk.Tk()
-app.title("Task Manager")
-
-# Run the main event loop
-app.mainloop()
-
-# Create a title label
-title_label = tk.Label(app, text="To-Do List", font=("Helvetica", 16, "bold"))
-
-# Pack the title label into the window
-title_label.pack(pady=10)
-
-# Step 3: Creating Entry Widget for Tasks
-# Create an entry widget for tasks
-task_entry = tk.Entry(app, width=40, font=('Helvetica', 12))
-
-# Pack the task entry widget into the window
-task_entry.pack(pady=10)
+ft.app(target=main)
