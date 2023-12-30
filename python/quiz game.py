dd227999206e41ead1f6b924d2f835e3c2dea5ac
Michaelunkai <michaelovsky5@gmail.com>
2023-12-31 00:14:51 +0200

    quiz

---
+++ python/quiz game.py
@@ -0,0 +1,26 @@
+# quiz game
+#------------------------------
+def new_game():
+    pass
+#------------------------------
+def check_answer():
+    pass
+#------------------------------
+def display_score():
+    pass
+#------------------------------
+def play_again():
+    pass
+#------------------------------
+
+questions = {
+    "who create Python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. mark Zockerburg"],
+          ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+          ["A. lonley Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+          ["A. True", "B. False", "C. sometimes", "D. what's Earth?"]]
