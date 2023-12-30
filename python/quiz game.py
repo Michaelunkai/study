668419f539c8356742876fecedab958a739c7d5d
Michaelunkai <michaelovsky5@gmail.com>
2023-12-31 01:16:15 +0200

    3

--- python/quiz game.py
+++ python/quiz game.py
@@ -157,6 +157,10 @@ def check_answer(answer, guess):
     
     if answer == guess:
         print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
 #------------------------------
 def display_score():
     pass
