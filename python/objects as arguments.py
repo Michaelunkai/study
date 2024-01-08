17c3e9a4d965701f3ca5fbf91a5760866423752e
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 21:49:17 +0200

    go

---
+++ python/objects as arguments.py
@@ -0,0 +1,16 @@
+# objects as arguments
+class Car:
+
+    color = None
+
+def change_color(car,color):
+
+    car.color = color
+
+car_1 = Car()
+car_2 = Car()
+car_3 = Car()
+
+print(car_2.color)
+print(car_2.color)
+print(car_2.color)
