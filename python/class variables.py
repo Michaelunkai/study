c4d4e0bc139a152d179b718b680e7f60c18aeabc
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 12:14:34 +0200

    output: 4 2 2

--- python/class variables.py
+++ python/class variables.py
@@ -17,3 +17,11 @@ class Car:
 # print(car_2.wheels)
 
 print(Car.wheels)
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+Car.wheels = 2
+
+print(car_1.wheels)
+print(car_2.wheels)
