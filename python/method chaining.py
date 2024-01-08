3f10d7d05676b65fc4362195e696303c111a6542
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 14:25:21 +0200

    chaining

---
+++ python/method chaining.py
@@ -0,0 +1,17 @@
+# method chaining
+# Calling multiple methods in a single line, each 
+# affecting the object, enabling concise code.
+
+class car:
+
+    def turn_on(self):
+        print("You start the engine")
+
+    def drive(self):
+        print("You drive the car")
+
+    def brake(self):
+        print("You step on the brakes")
+
+    def turn_off(self):
+        print("You turn off the engine")
