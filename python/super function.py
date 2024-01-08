4893905835986573f26f95eb684bf0cd89afd199
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 14:44:25 +0200

    2

--- python/super function.py
+++ python/super function.py
@@ -2,17 +2,22 @@
 # super() calls parent class methods, facilitating inheritance and avoiding hardcoding class names.
 
 class Rectangle:
-    pass
-
-class Square(Rectangle):
-
+    
     def __init__(self, length, width):
         self.length = length
         self.width = width
 
+class Square(Rectangle):
+
+    def __init__(self, length, width):
+        super().__init__(length,width)
+
+
 class Cube(Rectangle):
 
     def __init__(self, length, width, height):
-        self.length = length 
-        self.width = width
+        super().__init__(length,width)
         self.height = height
+
+Square = Square(3, 3)
+cube = Cube(3, 3, 3)
