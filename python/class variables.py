9bf39aa446389c54adfe7df42b76b6923fb8ec99
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 12:06:37 +0200

    variables

---
+++ python/class variables.py
@@ -0,0 +1,16 @@
+# class variables
+
+    wheels = 4 #class varaible
+
+class car:
+
+    def __init__(self,make,model,year,color):
+        self.make = make  #instance variable
+        self.model = model #instance variable
+        self.year = year #instance variable
+        self.color = color #instance variable
+
+from car import Car
+
+car_1 = Car("Chevy","Corvette",2021,"blue")
+car_2 = Car("Ford","Mustang",2022,"red")
