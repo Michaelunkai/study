88d035e222d4f47e4e7ccb7656f6288298a27237
Michaelunkai <michaelovsky5@gmail.com>
2024-01-08 21:52:22 +0200

    # output: # red # white # blue

--- python/objects as arguments.py
+++ python/objects as arguments.py
@@ -11,11 +11,16 @@ car_1 = Car()
 car_2 = Car()
 car_3 = Car()
 
-print(car_2.color)
-print(car_2.color)
-print(car_2.color)
+change_color(car_1,"red")
+change_color(car_2,"white")
+change_color(car_3,"blue")
 
-# output: 
-# None
-# None
-# None
+
+print(car_1.color)
+print(car_2.color)
+print(car_3.color)
+
+# output:
+# red
+# white
+# blue
