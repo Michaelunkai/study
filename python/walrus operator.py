d2f3f0edb211fdd407a9a28f3cd36352fd698fec
Michaelunkai <michaelovsky5@gmail.com>
2024-01-12 15:56:04 +0200

    with walrus

--- python/walrus operator.py
+++ python/walrus operator.py
@@ -2,12 +2,15 @@
 # assignment expression aka walrus operator
 # assign values to variables as part of a larger expression
 
+# foods = list()
+# while True:
+#     food = input("What food do youl like?: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+# now the same, using walrus operator:
+
 foods = list()
-while True:
-    food = input("What food do youl like?: ")
-    if food == "quit":
-        break
+while food := input("what food do you like? : ") != "quit":
     foods.append(food)
-# output: What food do youl like?: sushi
-# What food do youl like?: pizza
-# What food do youl like?: cheese
