e7b34f45ed785fdedcc0ffa528f79b8417fef0a0
Michaelunkai <michaelovsky5@gmail.com>
2024-01-12 15:53:46 +0200

    # output: What food do youl like?: sushi # What food do youl like?: pizza # What food do youl like?: cheese

--- python/walrus operator.py
+++ python/walrus operator.py
@@ -1,7 +1,13 @@
-# walrus operator
+# walrus operator :=
 # assignment expression aka walrus operator
 # assign values to variables as part of a larger expression
 
-happy = True
-print(happy)
-
+foods = list()
+while True:
+    food = input("What food do youl like?: ")
+    if food == "quit":
+        break
+    foods.append(food)
+# output: What food do youl like?: sushi
+# What food do youl like?: pizza
+# What food do youl like?: cheese
