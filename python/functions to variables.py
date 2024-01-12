3e0602422c60e649d61b51b9cb0859ad8d942459
Michaelunkai <michaelovsky5@gmail.com>
2024-01-12 16:00:15 +0200

    output: <function hello at 0x7feb3feec1f0> # <function hello at 0x7feb3feec1f0>

---
+++ python/functions to variables.py
@@ -0,0 +1,10 @@
+# functions to variables
+def hello():
+    print("Hello") 
+
+print(hello)
+hi = hello
+print(hi)   
+
+# output: <function hello at 0x7feb3feec1f0>
+# <function hello at 0x7feb3feec1f0>
