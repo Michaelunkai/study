54af0383b9ddfcbd173e300759857ca69e2d0c37
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 12:22:41 +0200

    10

---
+++ python/lambda function.py
@@ -0,0 +1,12 @@
+# lambda function
+# function written in 1 line using lambda keyword
+# accepts any number of arguments, but only has one expression. (like a shortcut)
+# useful if needed for a short period of time, throw-away)
+
+# syntax:
+#  lamnda parameters: expression
+def double(x):
+    return x * 2
+
+print(double(5))
+# output: 10
