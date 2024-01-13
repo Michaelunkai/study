6818ea964238f4f08adb018ecf99953c3f10ca97
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 11:52:30 +0200

    higher

---
+++ python/higher order functions.py
@@ -0,0 +1,14 @@
+# higher order functions
+#  a function that either:
+# 1. accepts a function as an argument
+# or 
+# 2. return a function
+#  (in python, functions are also treated as objects)
+
+# example:
+
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
