464f0dfcbc0c65489b942a5b89137f5271eb5acf
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 17:54:07 +0200

    tuple exapmle

---
+++ python/map.py
@@ -0,0 +1,9 @@
+# map
+# applies a function ti each item in an iterable (list, tuple, etc)
+
+# map(function, iterable)
+
+store = [("shirts",20.00),
+         ("pants",25.00),
+         ("jacket",50.00),
+         ("socks"10.00)]
