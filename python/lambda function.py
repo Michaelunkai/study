39ed2497241746fca9ccfcc851c68ad67558c545
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 12:31:54 +0200

    age check

--- python/lambda function.py
+++ python/lambda function.py
@@ -15,6 +15,6 @@ double = lambda x:x * 2
 multiply = lambda x, y: x * y
 add = lambda x, y, z: x + y + z
 full_name = lambda first_name, last_name: first_name+" "+last_name
-print(full_name("Misha", "Fedro"))
-
-# output: Misha Fedro
+age_check = lambda age:True if age >= 18 else False
+print(age_check(12))
+# output: False
