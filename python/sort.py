aaff16d2235bc259bcd1d78f68c8028e6569c527
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 15:00:55 +0200

    list of tuples

--- python/sort.py
+++ python/sort.py
@@ -3,10 +3,15 @@
 # sort() method = used with lists
 # sort() function = used with iterables
 
-students = ["Squidwards","Sandy","Patrick"] 
+# students = ["Squidwards","Sandy","Patrick"] 
 
-students.sort(reverse=True)
+# # students.sort(reverse=True)
+# sorted_students = sorted(students)
 
-for i in students:
-    print(i)
-# this will output in reverse alphabetical order!
+# for i in sorted_students:
+#     print(i)
+
+students = [("Squidward", "F", 60),
+            ("Sandy", "A", 33),
+            ("Patrick","D", 36)]
+
