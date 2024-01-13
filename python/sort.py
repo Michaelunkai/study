0e68d80e54efb903d1c806044538b7a95955e34e
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 14:53:42 +0200

    sort my list alphabeticly

---
+++ python/sort.py
@@ -0,0 +1,12 @@
+# sort
+
+# sort() method = used with lists
+# sort() function = used with iterables
+
+students = ["Squidwards","Sandy","Patrick"] 
+
+students.sort()
+
+for i in students:
+    print(i)
+# this will sort my list alphabeticly 
