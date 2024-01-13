36e5a72a0d1bf00ad0ec50c6839d548e1fcef86d
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 18:10:21 +0200

    convert usd to euros

--- python/map.py
+++ python/map.py
@@ -3,20 +3,41 @@
 
 # map(function, iterable)
 
-store = [("shirts",20.00),
-         ("pants",25.00),
-         ("jacket",50.00),
-         ("socks",10.00)]
+# store = [("shirts",20.00),
+#          ("pants",25.00),
+#          ("jacket",50.00),
+#          ("socks",10.00)]
 
-to_euros = lambda data: (data[0],data[1]*0.82)
+# to_euros = lambda data: (data[0],data[1]*0.82)
 
-store_euros = list(map(to_euros, store))
+# store_euros = list(map(to_euros, store))
 
-for i in store_euros:
-    print(i)
+# for i in store_euros:
+#     print(i)
 # output:
 # ('shirts', 16.4)
 # ('pants', 20.5)
 # ('jacket', 41.0)
 # ('socks', 8.2)
     
+# convert from euros to usd:
+    
+store = [("shirts",20.00),
+         ("pants",25.00),
+         ("jacket",50.00),
+         ("socks",10.00)]
+
+to_euros = lambda data: (data[0],data[1]*0.82)
+to_dollars =  lambda data: (data[0],data[1]/0.82)
+
+
+store_dollars = list(map(to_euros, store))
+
+for i in store_dollars:
+    print(i)
+
+# output:
+# ('shirts', 16.4)
+# ('pants', 20.5)
+# ('jacket', 41.0)
+# ('socks', 8.2)
