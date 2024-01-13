25280e72324b4198ad95024e38d01b81d70c1049
Michaelunkai <michaelovsky5@gmail.com>
2024-01-13 15:04:24 +0200

    sort

--- python/sort.py
+++ python/sort.py
@@ -11,7 +11,37 @@
 # for i in sorted_students:
 #     print(i)
 
+# students = [("Squidward", "F", 60),
+#             ("Sandy", "A", 33),
+#             ("Patrick","D", 36)]
+
+# students.sort()
+
+# for i in students:
+#     print(i)
+
+# output: 
+#     ('Patrick', 'D', 36)
+# ('Sandy', 'A', 33)
+# ('Squidward', 'F', 60)
+
+# students = [("Squidward", "F", 60),
+#             ("Sandy", "A", 33),
+#             ("Patrick","D", 36)]
+
+# grade = lambda grades:grades[1]
+# students.sort(key=grade)
+
+# for i in students:
+#     print(i)
+
+# in reverase:
 students = [("Squidward", "F", 60),
             ("Sandy", "A", 33),
             ("Patrick","D", 36)]
 
+grade = lambda grades:grades[1]
+students.sort(key=grade,reverse=True)
+
+for i in students:
+    print(i)
