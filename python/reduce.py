f344388e7b190bdbb18d26094243e9ffa31f8b7b
Michaelunkai <michaelovsky5@gmail.com>
2024-01-14 13:08:39 +0200

    120

--- python/reduce.py
+++ python/reduce.py
@@ -4,8 +4,15 @@
 
 # redice function
 
-import functools
+# import functools
 
-letters = ["H","E","L","L","O"]
-word = functools.reduce(lambda x, y,:x + y,letters)
-print(word)
+# letters = ["H","E","L","L","O"]
+# word = functools.reduce(lambda x, y,:x + y,letters)
+# print(word)
+# output: HELLO
+
+import functools
+factorial = [5,4,3,2,1]
+result = functools.reduce(lambda x, y,:x * y,factorial)
+print(result)
+# output: 120
