71e57288d5ce9362806117b351f0e6d64b6b5984
Michaelunkai <michaelovsky5@gmail.com>
2024-01-14 16:29:22 +0200

    with list comprehension

--- python/list comprehension.py
+++ python/list comprehension.py
@@ -3,8 +3,13 @@
 #  loops and conditions, 
 # making code efficient and readable.
 
-squares = [] #create an empty list
-for i in range(1,11): #create a for loop
-    squares.append(i * i) # define what each loop iteration should be
-print(squares)
+# squares = [] #create an empty list
+# for i in range(1,11): #create a for loop
+#     squares.append(i * i) # define what each loop iteration should be
+# print(squares)
 # output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# same, with list comprehension:
+
+squares = [i * i for i in range(1,11)]
+print(squares)
