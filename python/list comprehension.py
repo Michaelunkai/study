6a4da8a663df4668f16e5e62c207efea8ed42ecb
Michaelunkai <michaelovsky5@gmail.com>
2024-01-14 16:26:54 +0200

    [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]

---
+++ python/list comprehension.py
@@ -0,0 +1,10 @@
+# list comprehension
+# List comprehension in Python is concise syntax for creating lists, combining
+#  loops and conditions, 
+# making code efficient and readable.
+
+squares = [] #create an empty list
+for i in range(1,11): #create a for loop
+    squares.append(i * i) # define what each loop iteration should be
+print(squares)
+# output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
