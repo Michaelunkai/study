1643b5969ae3f86d648ac4f80a7683082b0f774b
Michaelunkai <michaelovsky5@gmail.com>
2024-01-15 15:28:34 +0200

    last example

--- python/zip function.py
+++ python/zip function.py
@@ -1,54 +1,70 @@
-# zip function
+# # zip function
 
-# The zip function in Python
-# combines corresponding elements from multiple 
-# iterables into tuples,
-# creating an iterator that produces pairs
-# of elements.
+# # The zip function in Python
+# # combines corresponding elements from multiple 
+# # iterables into tuples,
+# # creating an iterator that produces pairs
+# # of elements.
 
-# usernames = ["Dude", "Bro", "Mister"]
-# passwords = ("password","Abc", "guest")
+# # usernames = ["Dude", "Bro", "Mister"]
+# # passwords = ("password","Abc", "guest")
 
-# to zip elements from each iterable togheter,so thair in pairs,
-#  and each pair is going to be stored as a tuple within a zip object:
+# # to zip elements from each iterable togheter,so thair in pairs,
+# #  and each pair is going to be stored as a tuple within a zip object:
 
-# users = zip(usernames, passwords)
+# # users = zip(usernames, passwords)
 
-# for i in users:
-#     print(i)
+# # for i in users:
+# #     print(i)
 
-# output:
-# ('Dude', 'password')
-# ('Bro', 'Abc')
-# ('Mister', 'guest')
+# # output:
+# # ('Dude', 'password')
+# # ('Bro', 'Abc')
+# # ('Mister', 'guest')
     
-# we got a zip object of tuples! and each tuple store each pair of elemts of my 2 itetables/
+# # we got a zip object of tuples! and each tuple store each pair of elemts of my 2 itetables/
 
+
+
+# # usernames = ["Dude", "Bro", "Mister"]
+# # passwords = ("password","Abc", "guest")
+
+# # users = list(zip(usernames, passwords))
+
+# # for i in users:
+# #     print(i)
+
+# # output: 
+# # ('Dude', 'password')
+# # ('Bro', 'Abc')
+# # ('Mister', 'guest')
 
 
 # usernames = ["Dude", "Bro", "Mister"]
 # passwords = ("password","Abc", "guest")
 
-# users = list(zip(usernames, passwords))
+# users = dict(zip(usernames, passwords))
 
-# for i in users:
-#     print(i)
+# for key,value in users.items():
+#     print(key+" : "+value)
 
-# output: 
-# ('Dude', 'password')
-# ('Bro', 'Abc')
-# ('Mister', 'guest')
+# # output:
+# # Dude : password
+# # Bro : Abc
+# # Mister : guest
 
+# ---------------------------------------------------
 
 usernames = ["Dude", "Bro", "Mister"]
 passwords = ("password","Abc", "guest")
+login_date = ["1/1/2021","1/2/2021","1/3/2021"]
 
-users = dict(zip(usernames, passwords))
+users = zip(usernames,passwords,login_date)
 
-for key,value in users.items():
-    print(key+" : "+value)
+for i in users:
+    print(i)
 
 # output:
-# Dude : password
-# Bro : Abc
-# Mister : guest
+# ('Dude', 'password', '1/1/2021')
+# ('Bro', 'Abc', '1/2/2021')
+# ('Mister', 'guest', '1/3/2021')
