1e5c632d0bcfe386b8eaedd456992f952de30e7c
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 22:25:27 +0200

    # this is the time it took my program to count ftom 0 to 100000000

--- python/multiprocessingbro.py
+++ python/multiprocessingbro.py
@@ -11,17 +11,23 @@
 # sharing the same memory space, suitable
 # for I/O-bound tasks. Multiprocessing uses 
 # separate memory, ideal for CPU-bound tasks.
-
 from multiprocessing import Process, cpu_count
 import time
 
 def counter(num):
     count = 0
-    while  count < num:
+    while count < num:
         count += 1
 
 def main():
-    pass
+    a = Process(target=counter, args=(100000000,))
+    a.start()
+    a.join()
+    print("Finished in:", time.perf_counter(), "seconds")
 
 if __name__ == '__main__':
     main()
+
+# output: 
+    # Finished in: 1298.185145116 seconds
+    # this is the time it took my program to count ftom 0 to 100000000
