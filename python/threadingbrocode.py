fbf6f8765d1a859841b5acb5b87756a5e2139826
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 15:01:55 +0200

    2

--- python/threadingbrocode.py
+++ python/threadingbrocode.py
@@ -23,3 +23,26 @@
 # print(threading.enumerate())
 # # output : 1
 # # [<_MainThread(MainThread, started 140042503404416)>]
+
+# with multithreathing
+
+import threading
+import time
+
+def eat_breakfast():
+    time.sleep(3)
+    print("you eat breakfast")
+
+def drink_coffee():
+    time.sleep(4)
+    print("You drank coffee")
+
+def study():
+    time.sleep(5)
+    print("You study")
+
+print(threading.active_count())
+print(threading.enumerate())
+
+# output: 1
+# [<_MainThread(MainThread, started 140313416461184)>]
