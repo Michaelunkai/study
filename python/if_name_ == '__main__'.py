a2e112e7babca67425e1fb2c06da219fa8637914
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 12:44:37 +0200

    print(__name__)

--- python/if_name_ == '__main__'.py
+++ python/if_name_ == '__main__'.py
@@ -1,8 +1,11 @@
-# if_name_ == '__main__'
-# if __name__ == '__main__' checks if the Python script is being run directly,
-# allowing or preventing specific
-# code from executing when the script
-# is imported.
+# # if_name_ == '__main__'
+# # if __name__ == '__main__' checks if the Python script is being run directly,
+# # allowing or preventing specific
+# # code from executing when the script
+# # is imported.
 
-if __name__ == '__main__':
-    pass
+# if __name__ == '__main__':
+#     pass
+
+print(__name__)
+# output: __main__
