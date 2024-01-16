ede182bfc4f599481bff0ae770a6ec21299f8e9f
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 12:43:42 +0200

    pass

---
+++ python/if_name_ == '__main__'.py
@@ -0,0 +1,8 @@
+# if_name_ == '__main__'
+# if __name__ == '__main__' checks if the Python script is being run directly,
+# allowing or preventing specific
+# code from executing when the script
+# is imported.
+
+if __name__ == '__main__':
+    pass
