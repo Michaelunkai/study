e7adabaa2fc249d0995bf02591f68af821ff6843
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 14:57:52 +0200

    print(threading.enumerate())

--- python/threadingbrocode.py
+++ python/threadingbrocode.py
@@ -1,17 +1,25 @@
-# threading
-# Threading in Python allows
-# concurrent execution of tasks,
-# improving performance by running
-# multiple threads simultaneously 
-# within a single process.
+# # # threading
+# # # Threading in Python allows
+# # # concurrent execution of tasks,
+# # # improving performance by running
+# # # multiple threads simultaneously 
+# # # within a single process.
 
-# CPU-bound: Task limited by processor speed, computation-heavy,
-# minimal I/O operations.
+# # # CPU-bound: Task limited by processor speed, computation-heavy,
+# # # minimal I/O operations.
 
-# I/O-bound: Task constrained by input/output operations, frequent data access,
-#  waiting for external resources.
+# # # I/O-bound: Task constrained by input/output operations, frequent data access,
+# # #  waiting for external resources.
 
-import threading
-import time
-print(threading.active_count())
-# output: 1
+# # import threading
+# # import time
+# # print(threading.active_count())
+# # # output: 1
+
+# import threading
+# import time
+
+# print(threading.active_count())
+# print(threading.enumerate())
+# # output : 1
+# # [<_MainThread(MainThread, started 140042503404416)>]
