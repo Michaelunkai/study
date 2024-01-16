fd8aa49b60eb45143ae290ce3dcf67d12274521b
Michaelunkai <michaelovsky5@gmail.com>
2024-01-16 13:02:32 +0200

    epoch time

--- python/time module.py
+++ python/time module.py
@@ -5,6 +5,17 @@
 # execution time, and formatting
 # timestamps.
 
+# Prints Epoch time as a string:
 import time
 
-print()
+print(time.ctime(0))
+# output: Thu Jan  1 00:00:00 1970
+
+# Epoch refers to the starting point for
+# measuring time in computing.
+# It is often set to
+# January 1, 1970 (00:00:00 UTC),
+# and time is represented as the
+# number of seconds elapsed
+# since then.
+
