d016d00aced9c97eb53bd6d12678c5873cfd8cc9
Michaelunkai <michaelovsky5@gmail.com>
2024-01-20 14:04:04 +0200

    most simple tk gui

---
+++ python/GUI windows.py
@@ -0,0 +1,13 @@
+# GUI windows
+
+from tkinter import *
+
+# windows vs wighets:
+# widgets = GUI elemets: buttons, textboxes, labels, images
+# windows = serves as a container to hold or contain these widgets
+
+# creating simple window:
+
+window = Tk()  #instantiate an instance if a window
+
+window.mainloop()
