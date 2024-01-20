3341686cfa978946e67413aa0d0a00674c28fc5f
Michaelunkai <michaelovsky5@gmail.com>
2024-01-20 18:05:55 +0200

    label.pack()

---
+++ python/lables.py
@@ -0,0 +1,12 @@
+# lables
+
+from tkinter import * 
+
+# label = an area widget that holds text and/or an image within a windows
+
+window = Tk()
+
+label = Label(window, text="Hello World")
+label.pack()
+
+window.mainloop()
