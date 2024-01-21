54dc822b1971f36811a116dab76306edf7c6b8ed
Michaelunkai <michaelovsky5@gmail.com>
2024-01-21 19:09:24 +0200

    entry box

---
+++ python/entry box.py
@@ -0,0 +1,11 @@
+# Python entry box
+
+from tkinter import *
+# entry widget = textbox that accepts a single line of user input
+
+window = Tk()
+
+entry = Entry(window)
+entry.pack()
+
+window.mainloop()
