48a64669814e4962f750c5b56ec74594be78690d
Michaelunkai <michaelovsky5@gmail.com>
2024-01-21 12:55:03 +0200

    count how many times clicked

--- python/buttons.py
+++ python/buttons.py
@@ -2,12 +2,16 @@
 
 from tkinter import *
 
+count = 0
+
 def click():
-    print("You clicked my button!")
+    global count
+    count+=1
+    print(count)
 
 window = Tk()
 # add png file to the same folder
-photo = PhotoImage(file='image.png')
+# photo = PhotoImage(file='image.png')
 
 Button = Button(window,
                 text="This is misha's text",
@@ -17,8 +21,8 @@ Button = Button(window,
                 bg="black",
                 activeforeground="#00FF00",
                 activebackground="black",
-                state=ACTIVE,
-                compound='bottom') #will set the png image under the text 
+                state=ACTIVE)
+                # compound='bottom') #will set the png image under the text 
 
 Button.pack()
 
