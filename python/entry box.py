ac0d132dc19d0d07d9e08bd6b362083ca885e773
Michaelunkai <michaelovsky5@gmail.com>
2024-01-21 19:14:23 +0200

    print("Hello "+username)

--- python/entry box.py
+++ python/entry box.py
@@ -3,10 +3,17 @@
 from tkinter import *
 # entry widget = textbox that accepts a single line of user input
 
+def submit():
+    username = entry.get()
+    print("Hello "+username)
+
 window = Tk()
 
 entry = Entry(window,
               font=("Ariel",50))
-entry.pack()
+entry.pack(side=LEFT)
+
+submit_button = Button(window,text="submit",command=submit)
+submit_button.pack()
 
 window.mainloop()
