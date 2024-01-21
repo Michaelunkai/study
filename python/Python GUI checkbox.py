d498c04beee839f912ab49fc9cc482e8438845ac
Michaelunkai <michaelovsky5@gmail.com>
2024-01-21 22:34:51 +0200

    def display():     if(x.get()==1):         print("You agree!")     else:         print("You dont agree! ")

--- python/Python GUI checkbox.py
+++ python/Python GUI checkbox.py
@@ -1,9 +1,21 @@
 # Python GUI checkbox
 from tkinter import *
 
+def display():
+    if(x.get()==1):
+        print("You agree!")
+    else:
+        print("You dont agree! ")
+
 window = Tk()
 
+x = IntVar()
+
 check_button = Checkbutton(window,
-                           text="i Agree to something")
+                           text="i Agree to something",
+                           variable=x,
+                           onvalue=1,
+                           offvalue=0,
+                           command=display)
 check_button.pack()
 window.mainloop()
