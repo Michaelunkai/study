8d1a8918e5c9aebfd3dfd623f25248d72052adc6
Michaelunkai <michaelovsky5@gmail.com>
2024-01-21 02:06:27 +0200

    splunk

--- powershell/scripts/dockerdesktop.ps1
+++
@@ -1,4 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerDesktopInstaller.exe
-Start-Process -Wait -FilePath .\DockerDesktopInstaller.exe
-Remove-Item -Path .\DockerDesktopInstaller.exe
