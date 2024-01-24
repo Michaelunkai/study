29b4b84415c737b4806dfbce479e611a1fe4e52a
Michaelunkai <michaelovsky5@gmail.com>
2024-01-24 18:15:40 +0200

    2

--- python/from tkinter import messagebox.py
+++ python/from tkinter import messagebox.py
@@ -49,3 +49,39 @@ if response:
     print("You did a thing!")
 else:
     print("You cancled a thing!")
+
+# Step 5: Additional message boxes
+
+# Ask Retry, Cancel message box
+response = messagebox.askretrycancel("Ask Retry, Cancel", "Do you want to retry the thing?")
+if response:
+    print("You retried a thing")
+else:
+    print("You camceld a thing")
+
+# Ask Yes, No message box
+response = messagebox.askyesno("Ask Yes, No", "Do you like cake?")
+if response:
+    print("I like cake too")
+else:
+    print("Why do you not like cake?")
+
+# Ask Question message box
+answer = messagebox.askquestion("Ask Question", "Do you like pie?")
+if answer == "yes":
+    print("I like pie too")
+else:
+    print("Why do you not like pie?")
+
+# Step 6: Ask Yes, No, Cancel message box with icon
+answer = messagebox.askyesnocancel("Yes, No, Cancel", "Do you like to code?", icon='warning')
+if answer is True:
+    print("You like to code")
+elif answer is False:
+    print("Why are you watching a video on coding?")
+else:
+    print("You have dodged the question")
+
+# Step 7: Change the icon of the message box
+# Change icon to warning
+messagebox.showinfo("Custom Icon", "This has a warning icon", icon='warning')
