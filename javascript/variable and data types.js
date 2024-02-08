9abe4d71e9ceed5c8eeddd16964e12623f748996
Michaelunkai <michaelovsky5@gmail.com>
2024-02-08 13:04:24 +0200

    html

---
+++ javascript/variable and data types.js
@@ -0,0 +1,27 @@
+// a variable is container for storing data
+// a variable behaves as if it was the value that it contains
+
+// two steps:
+// 1. Declaration (var, let, const)
+// 2. Assignment (= assignment operator)
+
+// let age;
+// age = 21;
+// or:
+// let age = 21;
+// age = age +1;
+
+// string:
+
+// let firstName = "Misha"
+// let age = 21;
+
+// boolean:
+
+let firstName = "Misha"//strings 
+let age = 21;//numbers
+let student = false;//booleans
+
+console.log("Hello", firstName);
+console.log("You Are", age, "years old");
+console.log("Enrolled", student);
