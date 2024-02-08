b70d7c718ebf532484862bc3f6a2c5febfa76355
Michaelunkai <michaelovsky5@gmail.com>
2024-02-08 13:22:04 +0200

    Document  Hello Misha  You are 21 years old  Enrolled: false

--- javascript/variable and data types/main.js
+++ javascript/variable and data types/main.js
@@ -18,10 +18,18 @@
 
 // boolean:
 
-let firstName = "Misha"//strings 
-let age = 21;//numbers
-let student = false;//booleans
 
+// Declare variables
+let firstName = "Misha"; // strings
+let age = 21; // numbers
+let student = false; // booleans
+
+// Output to console
 console.log("Hello", firstName);
 console.log("You Are", age, "years old");
 console.log("Enrolled", student);
+
+// Output to HTML
+document.getElementById("p1").innerHTML = `Hello ${firstName}`;
+document.getElementById("p2").innerHTML = `You are ${age} years old`;
+document.getElementById("p3").innerHTML = `Enrolled: ${student}`;
