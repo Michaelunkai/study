fb1f78fad1737490f41e2978f8e6a311a8573994
Michaelunkai <michaelovsky5@gmail.com>
2024-02-12 15:07:27 +0200

    go

--- python/apps/movie-search/backend.py
+++ python/apps/movie-search/backend.py
@@ -1,30 +1,37 @@
 from elasticsearch import Elasticsearch
-# create an instance of Elasticsearch
-es = Elasticsearch()
+
+# Specify the Elasticsearch server's host and port
+es = Elasticsearch(["http://localhost:9200"])  # Assuming Elasticsearch is running on localhost with port 9200
+
 def search_movies(query):
-  # search movies using the Elasticsearch `search` method
-  # and return the search results
-  res = es.search(index="movies", body=query)
-  return res
+    # search movies using the Elasticsearch `search` method
+    # and return the search results
+    res = es.search(index="movies", body=query)
+    return res
+
 def filter_movies(name=None, actors=None, genre=None, date=None):
-  # create a bool query to filter the movies
-  bool_query = {
-    "bool": {
-      "must": []
+    # create a bool query to filter the movies
+    bool_query = {
+        "bool": {
+            "must": []
+        }
     }
-  }
-  # add filters to the bool query based on the provided parameters
-  if name:
-    bool_query["bool"]["must"].append({"match": {"name": name}})
-  if actors:
-    bool_query["bool"]["must"].append({"match": {"actors": actors}})
-  if genre:
-    bool_query["bool"]["must"].append({"match": {"genre": genre}})
-  if date:
-    bool_query["bool"]["must"].append({"match": {"release_date": date}})
-  # create the Elasticsearch query
-  query = {
-    "query": bool_query
-  }
-  # search and return the movies
-  return search_movies(query)
+    # add filters to the bool query based on the provided parameters
+    if name:
+        bool_query["bool"]["must"].append({"match": {"name": name}})
+    if actors:
+        bool_query["bool"]["must"].append({"match": {"actors": actors}})
+    if genre:
+        bool_query["bool"]["must"].append({"match": {"genre": genre}})
+    if date:
+        bool_query["bool"]["must"].append({"match": {"release_date": date}})
+    # create the Elasticsearch query
+    query = {
+        "query": bool_query
+    }
+    # search and return the movies
+    return search_movies(query)
+
+# Example usage:
+# res = filter_movies(name="The Matrix")
+# print(res)
--- python/apps/movie-search/frontend/src/MovieSearch.js
+++ python/apps/movie-search/frontend/src/MovieSearch.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
+
 const MovieSearch = () => {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+
   const searchMovies = () => {
     axios
       .get("http://localhost:5000/search", {
@@ -14,16 +16,42 @@ const MovieSearch = () => {
         setMovies(res.data.hits.hits);
       });
   };
+
+  const filterMovies = () => {
+    axios
+      .post("http://localhost:5000/filter", {
+        name: query,
+        actors: query,
+        genre: query,
+        date: query
+      })
+      .then(res => {
+        setMovies(res.data.hits.hits);
+      });
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      if (query) {
+        filterMovies();
+      } else {
+        setMovies([]);
+      }
+    }
+  };
+
   return (
     <div>
       <input
         type="text"
         value={query}
         onChange={event => setQuery(event.target.value)}
+        onKeyPress={handleKeyPress}
       />
       <button onClick={searchMovies}>Search</button>
       {movies.map(movie => (
         <div key={movie._id}>
+          <img src={movie.poster_url} alt={movie._source.name} />
           <h2>{movie._source.name}</h2>
           <p>Actors: {movie._source.actors}</p>
           <p>Genre: {movie._source.genre}</p>
@@ -33,4 +61,5 @@ const MovieSearch = () => {
     </div>
   );
 };
+
 export default MovieSearch;
