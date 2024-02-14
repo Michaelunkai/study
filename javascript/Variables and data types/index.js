f6ed9f295f90aaa17bb311ae94b2ddf0bf2a42a4
Michaelunkai <michaelovsky5@gmail.com>
2024-02-14 15:07:33 +0200

    finished

--- javascript/Variables and data types/index.js
+++ javascript/Variables and data types/index.js
@@ -43,9 +43,12 @@
 
 let firstName = "Misha";  //strings
 let age = 21; //numbers
-let student = false; //booleans
-
+let student = true; //booleans
 
 console.log("Hello", firstName);
 console.log("you are", age, "years old");
-console.log("Enrolled". student);
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
