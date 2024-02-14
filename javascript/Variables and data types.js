f4fa5f7cfdf5e6f38d7869954f985ef095f3785b
Michaelunkai <michaelovsky5@gmail.com>
2024-02-14 14:33:30 +0200

    let age

---
+++ javascript/Variables and data types.js
@@ -0,0 +1,6 @@
+// A variable is a container for storing data
+// a variable behaves as if it was the value that it contains
+
+// two steps:
+// 1. Declaration (car, let, const)
+// 2. Assignment (= assignment operator)
