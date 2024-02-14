41d73a8d022dbe0c085730317587001eeb66ee76
Michaelunkai <michaelovsky5@gmail.com>
2024-02-14 14:37:44 +0200

    strings numbers booleans

--- javascript/Variables and data types.js
+++ javascript/Variables and data types.js
@@ -4,3 +4,18 @@
 // two steps:
 // 1. Declaration (car, let, const)
 // 2. Assignment (= assignment operator)
+
+// let age;
+// age = 21;
+
+// or:
+// let age = 21;
+
+let firstName = "Misha";  //strings
+let age = 21; //numbers
+let student = true; //booleans
+// or = false;
+
+console.log(age);
+console.log(age);
+console.log(student);
