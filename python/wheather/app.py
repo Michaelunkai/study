d8b168b9abc2c28a91ba72ddfb702b5e62dc46c5
Michaelunkai <michaelovsky5@gmail.com>
2024-02-18 13:22:01 +0200

    add variable

--- python/wheather/app.py
+++ python/wheather/app.py
@@ -3,4 +3,7 @@ import requests
 api_key= 'dcd8c4f8fe9a2165bb574775b1a23ebc'
 
 user_input = input("Enter city: ")
-print(user_input)
+
+wheather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
