3174fa8421dd8434951b40ba15746423385efb9f
Michaelunkai <michaelovsky5@gmail.com>
2024-02-18 12:35:19 +0200

    convert string to a number

---
+++ javascript/type conversion.js
@@ -0,0 +1,9 @@
+// type Conversion = change the datatype of a value
+//  to another (strings, numbers, booleans)
+
+let age = window.prompt("How old are you?")
+
+age = Number(age);
+age += 1;
+
+console.log("Happy Birthday! you are", age, "years old");
