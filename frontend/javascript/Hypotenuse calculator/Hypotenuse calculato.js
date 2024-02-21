754adad4f644c1738756f9371192a4656118e54d
Michaelunkai <michaelovsky5@gmail.com>
2024-02-21 22:32:33 +0200

    1

---
+++ frontend/javascript/Hypotenuse calculator/Hypotenuse calculato.js
@@ -0,0 +1,13 @@
+let a;
+let b;
+let c;
+
+a = window.prompt("Enter side A")
+a = Number(a);
+
+b = window.prompt("Enter side B")
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("Side c:". c);
