732d039b2f6f39b86b98aa0f98cbc1e67414fccd
Michaelunkai <michaelovsky5@gmail.com>
2024-02-21 19:25:05 +0200

    k

--- python/sqliteMoviesGui.py
+++ python/sqliteMoviesGui.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect('wishlist.db')
 # Create a cursor object
 cursor = conn.cursor()
 
-# Define SQL queries to create tables for movies, TV shows, and games
+# Define SQL queries to create tables for movies, TV shows, games, and anime
 create_movies_table = """CREATE TABLE IF NOT EXISTS movies (
                             id INTEGER PRIMARY KEY,
                             title TEXT
@@ -24,10 +24,16 @@ create_games_table = """CREATE TABLE IF NOT EXISTS games (
                             title TEXT
                         )"""
 
+create_anime_table = """CREATE TABLE IF NOT EXISTS anime (
+                            id INTEGER PRIMARY KEY,
+                            title TEXT
+                        )"""
+
 # Execute SQL queries to create tables
 cursor.execute(create_movies_table)
 cursor.execute(create_tv_shows_table)
 cursor.execute(create_games_table)
+cursor.execute(create_anime_table)
 
 # Commit changes to the database
 conn.commit()
@@ -59,6 +65,15 @@ def add_game():
     conn.commit()
     messagebox.showinfo("Success", "Game(s) added successfully!")
 
+# Function to add a new anime or multiple anime to the database
+def add_anime():
+    titles = anime_entry.get("1.0", "end").split('\n')
+    for title in titles:
+        if title.strip():
+            cursor.execute("INSERT INTO anime (title) VALUES (?)", (title.strip(),))
+    conn.commit()
+    messagebox.showinfo("Success", "Anime(s) added successfully!")
+
 # Function to delete listings
 def delete_listings(category):
     confirm = messagebox.askyesno("Confirm Delete", f"Are you sure you want to delete all {category.replace('_', ' ')}?")
@@ -69,6 +84,8 @@ def delete_listings(category):
             cursor.execute("DELETE FROM tv_shows")
         elif category == "games":
             cursor.execute("DELETE FROM games")
+        elif category == "anime":
+            cursor.execute("DELETE FROM anime")
         
         conn.commit()
         messagebox.showinfo("Success", f"All {category.replace('_', ' ')} deleted successfully.")
@@ -89,6 +106,8 @@ def delete_selected_items():
                             cursor.execute("DELETE FROM tv_shows WHERE id=?", (item_id,))
                         elif category == "games":
                             cursor.execute("DELETE FROM games WHERE id=?", (item_id,))
+                        elif category == "anime":
+                            cursor.execute("DELETE FROM anime WHERE id=?", (item_id,))
                     conn.commit()
                     refresh_items_list(category)
                     messagebox.showinfo("Success", "Selected item(s) deleted successfully.")
@@ -106,6 +125,8 @@ def refresh_items_list(category):
             cursor.execute("SELECT * FROM tv_shows")
         elif category == "games":
             cursor.execute("SELECT * FROM games")
+        elif category == "anime":
+            cursor.execute("SELECT * FROM anime")
         items = cursor.fetchall()
         for item in items:
             items_listboxes[category].insert(tk.END, item[1])
@@ -119,12 +140,12 @@ def view_wishlist():
     view_window.configure(background="black")
 
     # Set the width and height of the view window
-    view_window_width = 800
+    view_window_width = 1000
     view_window_height = 600
     view_window.geometry(f"{view_window_width}x{view_window_height}")
 
     # Calculate the width for each listbox frame
-    frame_width = (view_window_width - 40) // 3
+    frame_width = (view_window_width - 60) // 4
 
     def remove_all_items(category):
         confirm = messagebox.askyesno("Confirm Remove All", f"Are you sure you want to remove all {category.replace('_', ' ')}?")
@@ -135,6 +156,8 @@ def view_wishlist():
                 cursor.execute("DELETE FROM tv_shows")
             elif category == "games":
                 cursor.execute("DELETE FROM games")
+            elif category == "anime":
+                cursor.execute("DELETE FROM anime")
             
             conn.commit()
             refresh_items_list(category)
@@ -227,19 +250,49 @@ def view_wishlist():
         game_label = tk.Label(game_frame, text="No games found", bg="black", fg="white")
         game_label.pack()
 
+    # Create a frame for anime
+    anime_frame = tk.Frame(view_window, bg="black", width=frame_width)
+    anime_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+    # Query all anime
+    cursor.execute("SELECT * FROM anime")
+    anime = cursor.fetchall()
+    if anime:
+        anime_label = tk.Label(anime_frame, text="Anime", font=("Helvetica", 16, "bold"), fg="red", bg="black")
+        anime_label.pack()
+        anime_list = tk.Listbox(anime_frame, height=20, width=30, bg="black", fg="white", selectmode=tk.MULTIPLE)
+        for ani in anime:
+            anime_list.insert(tk.END, ani[1])
+        anime_list.pack(fill=tk.BOTH, expand=True)
+        anime_scroll = tk.Scrollbar(anime_frame, orient=tk.VERTICAL)
+        anime_scroll.config(command=anime_list.yview)
+        anime_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        anime_list.config(yscrollcommand=anime_scroll.set)
+        items_listboxes["anime"] = anime_list
+        refresh_items_list("anime")
+
+        # Create Remove All button for anime
+        remove_anime_button = tk.Button(anime_frame, text="Remove All", command=lambda: remove_all_items("anime"), font=("Helvetica", 10), fg="red", bg="black")
+        remove_anime_button.pack(pady=5)
+
+    else:
+        anime_label = tk.Label(anime_frame, text="No anime found", bg="black", fg="white")
+        anime_label.pack()
+
 # Main window
 root = tk.Tk()
-root.title("Wishlist Manager")
+root.title("Michael Fedro's Wishlist Manager")
 root.configure(background="black")
-root.geometry("600x400")
 
 # Labels
 movie_label = tk.Label(root, text="Movie(s):", font=("Helvetica", 12, "bold"), fg="red", bg="black")
-movie_label.grid(row=0, column=0, padx=5, pady=5)
+movie_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 tv_show_label = tk.Label(root, text="TV Show(s):", font=("Helvetica", 12, "bold"), fg="red", bg="black")
-tv_show_label.grid(row=1, column=0, padx=5, pady=5)
+tv_show_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 game_label = tk.Label(root, text="Game(s):", font=("Helvetica", 12, "bold"), fg="red", bg="black")
-game_label.grid(row=2, column=0, padx=5, pady=5)
+game_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+anime_label = tk.Label(root, text="Anime(s):", font=("Helvetica", 12, "bold"), fg="red", bg="black")
+anime_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
 # Entry widgets
 movie_entry = tk.Text(root, height=5, width=30)
@@ -248,23 +301,26 @@ tv_show_entry = tk.Text(root, height=5, width=30)
 tv_show_entry.grid(row=1, column=1, padx=5, pady=5)
 game_entry = tk.Text(root, height=5, width=30)
 game_entry.grid(row=2, column=1, padx=5, pady=5)
+anime_entry = tk.Text(root, height=5, width=30)
+anime_entry.grid(row=3, column=1, padx=5, pady=5)
 
 # Buttons
-add_button = tk.Button(root, text="Add", width=10, command=add_movie)
-add_button.grid(row=0, column=2, padx=5, pady=5)
-add_button = tk.Button(root, text="Add", width=10, command=add_tv_show)
-add_button.grid(row=1, column=2, padx=5, pady=5)
-add_button = tk.Button(root, text="Add", width=10, command=add_game)
-add_button.grid(row=2, column=2, padx=5, pady=5)
+add_button = tk.Button(root, text="Add Movie", width=15, command=add_movie)
+add_button.grid(row=0, column=2, padx=5, pady=5, sticky="w")
+add_button = tk.Button(root, text="Add TV Show", width=15, command=add_tv_show)
+add_button.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+add_button = tk.Button(root, text="Add Game", width=15, command=add_game)
+add_button.grid(row=2, column=2, padx=5, pady=5, sticky="w")
+add_button = tk.Button(root, text="Add Anime", width=15, command=add_anime)
+add_button.grid(row=3, column=2, padx=5, pady=5, sticky="w")
 
 # View button
 view_button = tk.Button(root, text="See Wishlist", command=view_wishlist, font=("Helvetica", 12, "bold"), fg="red", bg="black")
-view_button.grid(row=3, column=0, columnspan=3, pady=10)
+view_button.grid(row=4, column=0, columnspan=3, pady=10, sticky="we")
 
 # Delete button
 delete_button = tk.Button(root, text="Delete Items", command=delete_selected_items, font=("Helvetica", 12, "bold"), fg="red", bg="black")
-delete_button.grid(row=4, column=0, columnspan=3, pady=10)
-
+delete_button.grid(row=5, column=0, columnspan=3, pady=10, sticky="we")
 
 # Dictionary to hold listboxes for each category
 items_listboxes = {}
