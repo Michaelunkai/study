39335fbb6cd3a454abb5c60902b95d181cf3bc48
Michaelunkai <michaelovsky5@gmail.com>
2024-02-25 20:47:01 +0200

    tetris + frogger

--- frontend/javascript/Hypotenuse calculator/index.js
+++ frontend/javascript/Hypotenuse calculator/index.js
@@ -1,7 +1,7 @@
 let a;
 let b;
 let c;
-
+/*
 a = window.prompt("Enter side A")
 a = Number(a);
 
@@ -11,3 +11,9 @@ b = Number(b);
 c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
 console.log("Side c:". c);
+*/
+
+document.getElementById("submitButton").oneclick = function(){
+
+    
+}
