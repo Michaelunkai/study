aae9aaf1882f619b7e3563bcd9256a3921777956
Michaelunkai <michaelovsky5@gmail.com>
2024-02-27 11:53:08 +0200

    tetris2.py

--- python/scripts/tetris/tetris2.py
+++ python/scripts/tetris/tetris2.py
@@ -252,16 +252,23 @@ def update_score(nscore):
         else:
             f.write(str(nscore))
 
-
 def max_score():
-    with open('scores.txt', 'r') as f:
-        lines = f.readlines()
-        score = lines[0].strip()
-
+    try:
+        with open('scores.txt', 'r') as f:
+            lines = f.readlines()
+            if lines:  # Check if there are any lines in the file
+                score = lines[0].strip()
+            else:
+                score = 0  # If the file is empty, set the score to 0
+    except FileNotFoundError:
+        with open('scores.txt', 'w') as f:
+            f.write('0')
+        score = 0
     return score
 
 
-def draw_window(surface, grid, score=0, last_score = 0):
+
+def draw_window(surface, grid, score=0, last_score=0):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
@@ -272,14 +279,14 @@ def draw_window(surface, grid, score=0, last_score = 0):
 
     # current score
     font = pygame.font.SysFont('comicsans', 30)
-    label = font.render('Score: ' + str(score), 1, (255,255,255))
+    label = font.render('Score: ' + str(score), 1, (255, 255, 255))
 
     sx = top_left_x + play_width + 50
-    sy = top_left_y + play_height/2 - 100
+    sy = top_left_y + play_height / 2 - 100
 
     surface.blit(label, (sx + 20, sy + 160))
     # last score
-    label = font.render('High Score: ' + last_score, 1, (255,255,255))
+    label = font.render('High Score: ' + str(last_score), 1, (255, 255, 255))
 
     sx = top_left_x - 200
     sy = top_left_y + 200
@@ -288,12 +295,13 @@ def draw_window(surface, grid, score=0, last_score = 0):
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size, block_size), 0)
+            pygame.draw.rect(surface, grid[i][j],
+                             (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size), 0)
 
     pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
 
     draw_grid(surface, grid)
-    #pygame.display.update()
+    # pygame.display.update()
 
 
 def main(win):  # *
@@ -317,10 +325,10 @@ def main(win):  # *
         level_time += clock.get_rawtime()
         clock.tick()
 
-        if level_time/1000 > 5:
+        if level_time / 1000 > 5:
             level_time = 0
-            if level_time > 0.12:
-                level_time -= 0.005
+            if fall_speed > 0.12:
+                fall_speed -= 0.005
 
         if fall_time/1000 > fall_speed:
             fall_time = 0
