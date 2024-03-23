546e4130030d4d25006f163d7788766fb29f0698
Michaelunkai <michaelovsky5@gmail.com>
2024-03-23 08:59:13 +0200

    resume 2th sketch

--- python/scripts/flaskresume/app.py
+++ python/scripts/flaskresume/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Replace this list with your skills
-    skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"]
+    skills = ["skills 1", "Skill 2", "Skill 3", "Skill 4","skill 5", "skill 6", "skill 7"]
     return render_template('index.html', skills=skills)
 
 if __name__ == '__main__':
