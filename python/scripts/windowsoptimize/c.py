2112b48b7fda5c046b1d5fec9a446c1eea2a9904
Michaelunkai <michaelovsky5@gmail.com>
2024-04-29 22:05:29 +0300

    j

--- python/scripts/windowsoptimize/c.py
+++ python/scripts/windowsoptimize/c.py
@@ -1,7 +1,7 @@
 import subprocess
 import ctypes
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QCheckBox, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTabWidget, QCheckBox, QPushButton
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QTimer
 
@@ -10,9 +10,9 @@ def run_command_as_admin(command):
         if sys.platform.startswith('win'):
             # Check if the script is running as admin
             if ctypes.windll.shell32.IsUserAnAdmin() != 0:
-                subprocess.run(["powershell", "-Command", command], shell=True, check=True)
+                subprocess.Popen(["powershell", "-NoExit", "-Command", command], shell=True)
             else:
-                ctypes.windll.shell32.ShellExecuteW(None, "runas", "powershell", "-Command " + command, None, 1)
+                ctypes.windll.shell32.ShellExecuteW(None, "runas", "powershell", "-NoExit -Command " + command, None, 1)
     except Exception as e:
         print("Error:", e)
 
@@ -24,14 +24,13 @@ def run_selected_commands():
 
     command_to_run = "; ".join(selected_commands)
     run_command_as_admin(command_to_run)
-    QTimer.singleShot(30000, close_powershell_window)
 
 def close_powershell_window():
-    run_command_as_admin("exit")
+    pass
 
 def choose_all():
     for var, cmd in zip(command_vars, commands):
-        if cmd["name"] not in ["Export WSL2 distros", "Turbo Mod", "PowerSaving Mod"]:
+        if cmd["name"] in bulk_commands:
             var.setChecked(True)
 
 if __name__ == "__main__":
@@ -49,6 +48,7 @@ if __name__ == "__main__":
         {"name": "windows updates", "command": "Install-Module -Name PSWindowsUpdate -Force -AllowClobber -Scope CurrentUser; Get-WindowsUpdate -Install -AcceptAll -Verbose"},
         {"name": "Defragment C Drive", "command": "defrag C: /U /V"},
         {"name": "Reset TCP/IP Stack", "command": "netsh int ip reset"},
+        {"name": "Reset/Renew IP", "command": "ipconfig /release; ipconfig /renew"},
         {"name": "Reset Winsock", "command": "netsh winsock reset"},
         {"name": "Analyze Component Store", "command": "dism /online /cleanup-image /analyzecomponentstore"},
         {"name": "Cleanup Component Store", "command": "dism /online /cleanup-image /startcomponentcleanup"},
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         {"name": "Clear Security Log", "command": "wevtutil cl Security"},
         {"name": "Clear System Log", "command": "wevtutil cl System"},
         {"name": "Clear DNS Cache", "command": "Clear-DnsClientCache"},
-        {"name": "Re-register AppX Packages", "command": "Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register \"$($_.InstallLocation)\\AppXManifest.xml\"}"},
+        {"name": "Reinstall Microsoft Store", "command": "Get-AppxPackage -allusers Microsoft.WindowsStore | foreach {Add-AppxPackage -register \"$($_.InstallLocation)\\appxmanifest.xml\" -DisableDevelopmentMode}"},
         {"name": "Start Defender Scan", "command": "Start-MpScan -ScanType QuickScan"},
         {"name": "Defender full Scan", "command": "Start-MpScan -ScanType FullScan"},
         {"name": "Unregister Kali WSL", "command": "wsl --unregister kali-linux"},
@@ -67,34 +67,57 @@ if __name__ == "__main__":
         {"name": "Export WSL2 distros", "command": "wsl --export kali-linux C:\\backup\\linux\\kalifull.tar; wsl --export ubuntu C:\\backup\\linux\\ubuntu.tar"},
         {"name": "Turbo Mod", "command": "python C:\\backup\\windowsapps\\powerplans\\turbo.py"},
         {"name": "PowerSaving Mod", "command": "python C:\\backup\\windowsapps\\powerplans\\powersavings.py"},
-        {"name": "Disable Windows Firewall", "command": 'netsh advfirewall set allprofiles state off && netsh firewall set opmode mode=disable && reg add "HKLM\\SOFTWARE\\Microsoft\\Windows Defender\\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f && reg add "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f'},
-        {"name": "Enable Windows Firewall", "command": 'netsh advfirewall set allprofiles state on && netsh firewall set opmode mode=enable && reg delete "HKLM\\SOFTWARE\\Microsoft\\Windows Defender\\Real-Time Protection" /v DisableRealtimeMonitoring /f && reg delete "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender" /v DisableAntiSpyware /f'}
+        {"name": "Disable Windows Firewall", "command": "Set-MpPreference -DisableRealtimeMonitoring $true; Set-NetFirewallProfile -Profile Domain, Private, Public -Enabled False"},
+        {"name": "Enable Windows Firewall", "command": "Set-MpPreference -DisableRealtimeMonitoring $false; Set-NetFirewallProfile -Profile Domain, Private, Public -Enabled True"},
+        {"name": "Enable SSH", "command": "Add-WindowsCapability -Online -Name OpenSSH.Server; Start-Service sshd; Set-Service -Name sshd -StartupType 'Automatic'; New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22; Get-Service sshd"},
+        {"name": "Ram Usage", "command": "$totalMemory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory; Get-Process | Sort-Object -Property WorkingSet -Descending | Select-Object Name, @{Name='MemoryUsage(MB)'; Expression={[math]::Round($_.WorkingSet / 1MB, 2)}}, @{Name='MemoryUsage(%)'; Expression={($_.WorkingSet / $totalMemory) * 100}}"},
+        {"name": "cleanmgr Tool", "command": "cleanmgr /sageset:1"},
     ]
 
     app = QApplication(sys.argv)
     root = QWidget()
     root.setWindowTitle("Select Commands to Run")
     root.setStyleSheet("background-color: #f5f5dc;")
-    layout = QVBoxLayout(root)
+
+    tab_widget = QTabWidget()
+    root_layout = QVBoxLayout(root)
+    root_layout.addWidget(tab_widget)
+
+    # Add the tab for commands not included in "Choose All"
+    not_bulk_tab = QWidget()
+    not_bulk_layout = QVBoxLayout()
+    not_bulk_tab.setLayout(not_bulk_layout)
+    tab_widget.addTab(not_bulk_tab, "Not Bulk")
+
+    # Add the tab for all other commands
+    bulk_tab = QWidget()
+    bulk_layout = QVBoxLayout()
+    bulk_tab.setLayout(bulk_layout)
+    tab_widget.addTab(bulk_tab, "Bulk")
 
     command_vars = []
+    bulk_commands = ["Update choco Packages", "Scan System Health", "Restore System Health", "Check System Files", "Check Image Health", "Restore Image Health", "Cleanup Component Store", "Start Update Service", "windows updates", "Defragment C Drive", "Reset TCP/IP Stack", "Reset Winsock", "Analyze Component Store", "Cleanup Component Store", "Flush DNS Cache", "Clear Application Log", "Clear Security Log", "Clear System Log", "Clear DNS Cache", "Start Defender Scan", "Unregister Kali WSL", "Import Kali WSL", "Unregister Ubuntu WSL", "Import Ubuntu WSL", "Reset/Renew IP", "cleanmgr Tool"]
     for cmd in commands:
         checkbox = QCheckBox(cmd['name'], parent=root)
         checkbox.setFont(QFont("Lobster", 10, QFont.Bold))
-        layout.addWidget(checkbox)
         command_vars.append(checkbox)
+
+        if cmd["name"] in bulk_commands:
+            bulk_layout.addWidget(checkbox)
+        else:
+            not_bulk_layout.addWidget(checkbox)
 
     choose_all_button = QPushButton("Choose All", parent=root)
     choose_all_button.clicked.connect(choose_all)
     choose_all_button.setStyleSheet("background-color: black; color: white; font-weight: bold;")
-    layout.addWidget(choose_all_button)
+    root_layout.addWidget(choose_all_button)
 
     run_button = QPushButton("Run Selected Commands", parent=root)
     run_button.clicked.connect(run_selected_commands)
     run_button.setStyleSheet("background-color: black; color: white; font-weight: bold;")
-    layout.addWidget(run_button)
+    root_layout.addWidget(run_button)
 
-    root.setLayout(layout)
+    root.setLayout(root_layout)
     root.showMaximized()  # Open in full-screen mode
 
     sys.exit(app.exec_())
