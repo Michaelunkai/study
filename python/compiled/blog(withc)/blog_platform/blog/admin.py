59202a9d756b9be7333edbd44f908f2bd14b73cc
michaelunkai <michaelovsky5@gmail.com>
2024-06-09 01:01:32 +0300

    programming

--- python/compiled/blog(withc)/blog_platform/blog/admin.py
+++
@@ -1,6 +0,0 @@
-#blog/admin.py
-
-from django.contrib import admin
-from .models import BlogPost
-
-admin.site.register(BlogPost)
--- python/compiled/blog(withc)/blog_platform/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import blog_list
-
-urlpatterns = [
-    path('list/', blog_list, name='blog_list'),
-]
